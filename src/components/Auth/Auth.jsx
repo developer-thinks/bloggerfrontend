b3884b66fc686b88b904ddc44379b68ab72f5aa0
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2022-02-28 21:56:25 +0530

    login and signup form with neat and clean code

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,10 +1,63 @@
 import React from 'react'
-import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
-
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import useStyles from './styles'
+import Input from './Input';
+import { useState } from 'react';
 
 const Auth = () => {
+
+  const classes = useStyles();
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  // const isSignUp = false;
+
+  const handleSybmit = (e) =>{
+      e.preventDefault();
+  }
+
+  const handleChange =()=>{
+
+  }
+
+  const switchMode = () =>{
+    setIsSignUp((prevState)=> !prevState)
+  }
+
   return (
-    <div>Auth</div>
+    <Container component="main" maxWidth="xs"> 
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant='h5'> {isSignUp ? 'SignUp' : 'SignIn'} </Typography>
+        <form className={classes.form} onSubmit={handleSybmit} >
+          <Grid container spacing={2} >
+            {
+              isSignUp &&(
+              <>
+                {/* <TextField label="first Name" name="firstname" handleChange={handleChange} autoFocus xs={6}  />   */}
+                <Input label="first Name" name="firstName" handleChange={handleChange} autoFocus half />
+                <Input name="lastName" label="Last Name" handleChange={handleChange} half />
+              </>
+            )}
+            <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+            <Input name="password" label="Password" handleChange={handleChange} />
+             { isSignUp && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
+          </Grid>
+           <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+            { isSignUp ? 'Sign Up' : 'Sign In' }
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                { isSignUp ? 'Already have an account? Sign in' : "Don't have an account? Sign Up" }
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
   )
 }
 
--- src/components/Auth/Input.js
+++ src/components/Auth/Input.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
-const Input = () => {
+
+const Input = ({name, handleChange, label, half, autoFocus, type, handleShowPassword }) => {
   return (
-    <div>Input</div>
+    <Grid item  xs={6} sm={half? 6 :12 }>
+      <TextField 
+        name={name}
+        onChange={handleChange}
+        required
+        variant='outlined'
+        fullWidth
+        label={label}
+        autoFocus={autoFocus}
+        type={type}
+        
+      />
+    </Grid>
   )
 }
 
