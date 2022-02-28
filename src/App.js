26166bc64b5b91ced444797937cbb452bc7f889c
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2022-02-28 16:04:53 +0530

    code is broken into more components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import Posts from './components/Posts/Posts.jsx'
-import Form from './components/Form/Form.jsx'
-import useStyles from './styles'
-import {getPosts} from './actions/posts'
-import {useDispatch} from 'react-redux'
-import { useEffect } from 'react';
-import { useState } from 'react';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar.jsx';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Auth from './components/Auth/Auth';
+
+
+
 
 function App() {
 
-  const [currentId, setCurrentId] = useState(null);
-
-  const classes = useStyles()
-  const dispatch = useDispatch()
-
-  useEffect(()=>{
-    dispatch(getPosts())
-  },[dispatch, currentId])
+  
 
   return (
-    <Container maxWidth ='lg'>
-      <AppBar className={classes.appBar} position='static' color='inherit'>
-        <Typography className={classes.heading} variant='h4' align='center' > Blogger </Typography>
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+    <Router>
+      <Container maxWidth ='lg'>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/auth' element={<Auth />} />
+        </Routes>
     </Container>
+    </Router>
   );
 }
 
