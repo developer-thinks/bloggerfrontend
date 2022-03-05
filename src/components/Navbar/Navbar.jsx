35e884f8635a39c346670d175b9e2476a421c756
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2022-03-05 10:51:43 +0530

    header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -37,14 +37,14 @@ const Navbar = () => {
   return (
     <AppBar className={classes.appBar} position='static' color='inherit'>
         <div className={classes.brandContainer}>
-          <Typography component={Link} to="/" className={classes.heading} variant='h4' align='center' >Samsquare 	 </Typography>  {/*&lt; &#47; &gt; */}
+          <Typography component={Link} to="/" className={classes.heading} variant='h4' align='center' >square 	 </Typography>  {/*&lt; &#47; &gt; */}
         </div>
         <div className={classes.toolbar}>
           {
             user ? (
                <div className={classes.profile}>
                 <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
-                <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
+                {/* <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography> */}
                 <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
           </div>
             ) :(
